62423d58d4cc8489c253d66900149a48827102e0
vash <trg_vash@A103-04L.imd.local>
2019-08-27 16:34:03 -0300

    Estados do peixe, alimentação feitos

--- scripts/Aquario.gd
+++ scripts/Aquario.gd
@@ -8,13 +8,21 @@ var foods  = []
 
 func _process(delta):
 	for fish in fishes:
+		var toRemove = []
+		var index    = -1
 		for food in foods:
+			index += 1
+			if food.eaten:
+				toRemove.push_front(index)
+				continue
 			var distance = (fish.position - food.position).length()
 			if distance <= fish.get_food_radar_size():
 				fish.look_at_food(food)
 			else:
 				fish.look_straight()
-		
+		for i in toRemove:
+			foods[i].queue_free()
+			foods.remove(i)
 	
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 2:
--- scripts/FishMovement.gd
+++ scripts/FishMovement.gd
@@ -7,14 +7,43 @@ var currentSpeed         = 50
 var lookingAtFood        = false
 var currentState         = "normal"
 var currentFood          = null
+var margin 				 = 28
+
+export (Texture) var normalTexture = null
+export (Texture) var deadTexture   = null
+export var maxEnergy      = 5 #o máximo de comida que o peixe pode comer
+export var energy         = 3 #quantidade de energial atual que o peixe tem
+export var energyDuration = 3.0
+
+var currentDuration = null #informa quanto tempo foi gasto na energia atual
+
+func _ready():
+	currentDuration = energyDuration
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	currentDuration -= delta
+	
+	if currentDuration <= 0:
+		energy -= 1
+		currentDuration = energyDuration
+	
+	if energy < 0:
+		currentState = "morto"
+	
+	if currentState == "morto":
+		texture = deadTexture
+	else:
+		texture = normalTexture
+	
 	if currentSpeed > normalSpeed:
 		currentSpeed = currentSpeed - (100 * delta)
 		
-	if position.x < 0 or position.x > 400:
+	if position.x < margin or position.x > (get_viewport().size.x - margin):
 		direction = direction * -1
+		
+	flip_h = false
+	flip_v = false
 	
 	if currentState == "normal":
 		normal_state(delta)
@@ -30,11 +59,17 @@ func _process(delta):
 	
 
 func normal_state(delta):
-	position.x = position.x + (currentSpeed * delta * direction)
+	position.x = position.x + (get_current_speed() * delta * direction)
+	flip_h = direction < 0
+	rotation_degrees = 0
 
 func watching_state(delta):
-	var foodDirection = (currentFood.position - position).normalized()
-	position += currentSpeed * delta * foodDirection
+	if currentFood.get_ref():
+		var foodDirection = (currentFood.get_ref().position - position).normalized()
+		position += get_current_speed() * delta * foodDirection
+	else:
+		currentFood  = null
+		currentState = "normal"	
 	
 func full_state(delta):
 	pass
@@ -44,15 +79,32 @@ func starving_state(delta):
 	
 func dead_state(delta):
 	flip_v = true
-	pass
+	flip_h = false
+	rotation_degrees = 0
+	if position.y < (get_viewport().size.y - margin):
+		position.y += 10 * delta
+
+func is_full():
+	return energy == maxEnergy
 	
+func is_hungry():
+	return energy == 0 or energy == 1
+
 func get_food_radar_size():
-	if currentState == "faminto":
-		return 2 * foodRadarSize
-	elif currentState == "cheio":
-		return 0.5 * foodRadarSize
+	if is_hungry():
+		return 4 * foodRadarSize
+	elif is_full():
+		return 0.25 * foodRadarSize
 	else:
 		return foodRadarSize
+		
+func get_current_speed():
+	if is_hungry():
+		return 4 * currentSpeed
+	elif is_full():
+		return 0.25 * currentSpeed
+	else:
+		return currentSpeed
 
 func _on_clicked(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
@@ -60,8 +112,10 @@ func _on_clicked(viewport, event, shape_idx):
 		currentSpeed = 400
 
 func look_at_food(food):
+	if currentState == "morto":
+		return
 	currentState = "observando"
-	currentFood  = food
+	currentFood  = weakref(food)
 	look_at(food.position)
 	flip_h = false
 	lookingAtFood = true
@@ -72,3 +126,12 @@ func look_straight():
 	rotation = 0
 	flip_v = false
 	lookingAtFood = false
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("food"):
+		area.eaten = true
+		energy += 1
+		if energy > maxEnergy:
+			currentState = "morto"
+		else:
+			currentState = "normal"
